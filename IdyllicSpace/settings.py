1f5371e1e23be99ccbbccf29d49a50d8c1d20388
nanps <pornlapas.nan@gmail.com>
2023-04-11 23:42:57 +0700

    microphone

--- IdyllicSpace/settings.py
+++ IdyllicSpace/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%o3@iazm#%mhom!he7-ndqkxc!tco@fo=0-y*jm&^bx@e4_t*u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['16ab-2001-fb1-66-984c-d9b1-e0fe-d9ab-ced7.ngrok-free.app']
+ALLOWED_HOSTS = []
 CSRF_TRUSTED_ORIGINS = ["https://*.ngrok-free.app"]
 
 LOGOUT_REDIRECT_URL = '/'
